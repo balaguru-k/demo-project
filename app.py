9b01c9726337c5b82127e4a833aaf037990d1ff0
balaguru-k <balaspeaks05@gmail.com>
2026-07-03 14:00:31 +0530

    demo project

---
+++ app.py
@@ -0,0 +1,6 @@
+def greet(name):
+    return f"Hello, {name}! Welcome to Jenkins CI/CD."
+
+if __name__ == "__main__":
+    print(greet("Bala"))
+    print("Python application executed successfully!")
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Python Jenkins Demo
+
+This is a simple Python project to demonstrate Continuous Integration (CI) using Jenkins.
+
+## Run Locally
+
+python app.py
