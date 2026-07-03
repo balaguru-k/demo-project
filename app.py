9a18f80c7aae93e6286f365f71a1fbb02167b38c
balaguru-k <144519978+balaguru-k@users.noreply.github.com>
2026-07-03 14:46:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-def greet(name):
-    return f"Hello, {name}! Welcome to Jenkins CI/CD."
-
-if __name__ == "__main__":
-    print(greet("Bala"))
-    print("Python application executed successfully!")
+print("hi")
